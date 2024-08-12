6459730adcd5ddbcb0b84f096596bede9fffb52d
Emanuel Claudino <emcti@Desk01>
2024-08-12 10:59:41 -0300

    feat: test

--- provider.tf
+++ provider.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.62.0"
     }
+  }
+  backend "s3" {
+    bucket = "emcti-devops-terraform"
+    key    = "dev2/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
